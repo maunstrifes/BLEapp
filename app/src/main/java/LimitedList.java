7eaf857f0168264c7f99a3f5a23e7d70fff22b6d
manu <manuel.heinzl@gmail.com>
2015-03-19 10:35:10 +0100

    Initial commit

---
+++ app/src/main/java/LimitedList.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+/**
+ * List with limited number of elements.
+ * If an element is added when the list is already full, the last element is removed.
+ *
+ * @author Manuel Heinzl
+ */
+public class LimitedList<E> extends LinkedList<E>{
+
+    private int limit;
+
+    public LimitedList(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public boolean add(E o) {
+        super.add(o);
+        while (size() > limit) { super.remove(); }
+        return true;
+    }
+}
